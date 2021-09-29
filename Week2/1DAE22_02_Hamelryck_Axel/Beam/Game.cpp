76e934ebb54db96f9939764f533de9a8e42642dd
akselele <ahamelryck@gmail.com>
2021-09-29 12:04:22 +0200

    beam

---
+++ Week2/1DAE22_02_Hamelryck_Axel/Beam/Game.cpp
@@ -0,0 +1,114 @@
+#include "pch.h"
+#include "Game.h"
+
+//Basic game functions
+#pragma region gameFunctions											
+void Start()
+{
+	// initialize game resources here
+}
+
+void Draw()
+{
+	ClearBackground(0,0,0);
+
+	// Put your own draw statements here
+	DrawBeam();
+}
+
+void Update(float elapsedSec)
+{
+	// process input, do physics 
+
+	// e.g. Check keyboard state
+	//const Uint8 *pStates = SDL_GetKeyboardState( nullptr );
+	//if ( pStates[SDL_SCANCODE_RIGHT] )
+	//{
+	//	std::cout << "Right arrow key is down\n";
+	//}
+	//if ( pStates[SDL_SCANCODE_LEFT] && pStates[SDL_SCANCODE_UP])
+	//{
+	//	std::cout << "Left and up arrow keys are down\n";
+	//}
+}
+
+void End()
+{
+	// free game resources here
+}
+#pragma endregion gameFunctions
+
+//Keyboard and mouse input handling
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key)
+{
+
+}
+
+void OnKeyUpEvent(SDL_Keycode key)
+{
+	//switch (key)
+	//{
+	//case SDLK_LEFT:
+	//	//std::cout << "Left arrow key released\n";
+	//	break;
+	//case SDLK_RIGHT:
+	//	//std::cout << "Right arrow key released\n";
+	//	break;
+	//case SDLK_1:
+	//case SDLK_KP_1:
+	//	//std::cout << "Key 1 released\n";
+	//	break;
+	//}
+}
+
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e)
+{
+	//std::cout << "  [" << e.x << ", " << e.y << "]\n";
+	//Point2f mousePos{ float( e.x ), float( g_WindowHeight - e.y ) };
+}
+
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e)
+{
+
+}
+
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e)
+{
+	////std::cout << "  [" << e.x << ", " << e.y << "]\n";
+	//switch (e.button)
+	//{
+	//case SDL_BUTTON_LEFT:
+	//{
+	//	//std::cout << "Left mouse button released\n";
+	//	//Point2f mousePos{ float( e.x ), float( g_WindowHeight - e.y ) };
+	//	break;
+	//}
+	//case SDL_BUTTON_RIGHT:
+	//	//std::cout << "Right mouse button released\n";
+	//	break;
+	//case SDL_BUTTON_MIDDLE:
+	//	//std::cout << "Middle mouse button released\n";
+	//	break;
+	//}
+}
+#pragma endregion inputHandling
+
+#pragma region ownDefinitions
+// Define your own functions here
+void DrawBeam()
+{
+	float squareSize{ 200.0f };
+	float border{ 10.0f };
+	SetColor(0, 128.0f/255, 0);
+	FillRect(border, border, squareSize, squareSize);
+	FillRect(border + squareSize, border + squareSize, squareSize, squareSize);
+	SetColor(1, 1, 1);
+	DrawRect(border, border, squareSize, squareSize);
+	DrawRect(border + squareSize, border + squareSize, squareSize, squareSize);
+	DrawLine(border, border, g_WindowHeight - 10, g_WindowWidth - 10);
+	DrawLine(border, border + 200, g_WindowHeight - 210, g_WindowWidth - 10);
+	DrawLine(border + 200, border, g_WindowHeight - 10, g_WindowWidth - 210);
+
+}
+#pragma endregion ownDefinitions
